57083a800a26d1011e75efb0b6abde7a3ce5b233
Jolene <sulfur.mermaid@gmail.com>
2018-11-20 18:38:46 -0500

    //ugh ok looks like i need to learn about asynchronous functions.  google it or whatev

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 
 const client = new Discord.Client();
 const config = require("./config.json");
+
 // We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
 client.config = config;
 
