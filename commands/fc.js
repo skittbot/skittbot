375ee2e677df5c81b1cb656e7cb513682686f336
J Cotnoir <sulfur.mermaid@gmail.com>
2018-11-26 22:20:15 -0500

    test to see how args do

--- commands/fc.js
+++ commands/fc.js
@@ -1,13 +1,15 @@
 exports.run = (client, message, args) => {
 
   if(!args || args.size < 1) return message.reply("My friend code is x");
+  console.log(args);
   switch(args[1]) {
 
     case args[1] === 'add':
       // adding a friend code to your account. syntax should be ""
 
+
     break;
-    
+
     case args[1] === 'del':
       // deleting a specific friend code
 
