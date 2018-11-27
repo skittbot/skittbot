7ed588f4afa524401c515eb0cbc360acd24c1675
J Cotnoir <sulfur.mermaid@gmail.com>
2018-11-27 00:38:10 -0500

    added del and delall.  they don't actually delete the user's ID from the system, just fill it with false info or remove it entirely.

--- commands/fc.js
+++ commands/fc.js
@@ -7,7 +7,7 @@ exports.run = (client, message, args) => {
 
     case 'add':
   // adding a friend code to your account. syntax should be ""
-      if (args.length !== 3) return message.reply("The syntax for this command is: !fc add [_console_] [_friend code_]");
+      if (args.length !== 3) return message.reply("the syntax for this command is: !fc add [_console_] [_friend code_]");
       let newConsole = args[1].toUpperCase();
       let newFC = args[2].replace(/\D/g,"");
       if (args[1] === 'WIIU') newFC = args[2].replace(/\W/g,"");
@@ -15,40 +15,65 @@ exports.run = (client, message, args) => {
       switch(newConsole) {
   // add new consoles as needed here
         case 'SWITCH':
-          if (newFC.length !== 12) return message.reply("Your friend code for the Nintendo Switch should be 12 digits.");
+          if (newFC.length !== 12) return message.reply("your friend code for the Nintendo Switch should be 12 digits.");
           finalFC = `SW-${newFC.slice(0,4)}-${newFC.slice(4,8)}-${newFC.slice(8,12)}`;
           client.fcs.ensure(message.author.id, {});
           client.fcs.set(message.author.id,finalFC,"switch");
           console.log(message.author.id+": "+client.fcs.get(message.author.id,"switch"));
           break;
         case '3DS':
-          if (newFC.length !== 12) return message.reply("Your friend code for the Nintendo 3DS should be 12 digits.");
+          if (newFC.length !== 12) return message.reply("your friend code for the Nintendo 3DS should be 12 digits.");
           finalFC = `${newFC.slice(0,4)}-${newFC.slice(4,8)}-${newFC.slice(8,12)}`;
           client.fcs.ensure(message.author.id, {});
           client.fcs.set(message.author.id,finalFC,"3ds");
           console.log(message.author.id+": "+client.fcs.get(message.author.id,"3ds"));
           break;
         case 'WIIU':
-          if (newFC.length > 15) return message.reply("Your Wii U account name can\'t be that long.");
+          if (newFC.length > 15) return message.reply("your Wii U account name can\'t be that long.");
           finalFC = newFC;
           client.fcs.ensure(message.author.id, {});
           client.fcs.set(message.author.id,finalFC,"wiiu");
           console.log(message.author.id+": "+client.fcs.get(message.author.id,"wiiu"));
           break;
         default:
-          return message.reply("You did not input a valid friend code. The syntax for this command is: !fc add [_console_] [_friend code_]");
+          return message.reply("you did not input a valid friend code. The syntax for this command is: !fc add [_console_] [_friend code_]");
           break;
         };
     break;
 
     case 'del':
       // deleting a specific friend code
-
+          if (args.length !== 2) return message.reply("the syntax for this command is: !fc del [_console_]");
+          let delConsole = args[1].toUpperCase();
+          switch(delConsole) {
+      // add new consoles as needed here
+            case 'SWITCH':
+              client.fcs.ensure(message.author.id, {});
+              client.fcs.set(message.author.id,false,"switch");
+              console.log(message.author.id+": "+client.fcs.get(message.author.id,"removed switch"));
+              break;
+            case '3DS':
+              client.fcs.ensure(message.author.id, {});
+              client.fcs.set(message.author.id,false,"3ds");
+              console.log(message.author.id+": "+client.fcs.get(message.author.id,"removed 3ds"));
+              break;
+            case 'WIIU':
+              client.fcs.ensure(message.author.id, {});
+              client.fcs.set(message.author.id,false,"wiiu");
+              console.log(message.author.id+": "+client.fcs.get(message.author.id,"removed wiiu"));
+              break;
+            default:
+              return message.reply("the console you specified could not be found. The syntax for this command is: !fc del [_console_]");
+              break;
+            };
 
     break;
 
     case 'delall':
       //delete all friend code data
+      client.fcs.ensure(message.author.id, {});
+      client.fcs.set(message.author.id,{});
+      return message.reply("your friend code data has been deleted.");
 
     break;
 
