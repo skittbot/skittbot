268cd5e5a55309fdce7afc40512fc216a26460d6
Jolene <sulfur.mermaid@gmail.com>
2019-01-08 09:45:56 -0500

    i don't even know any more?

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const client = new Discord.Client();
 const config = require("./config.json");
 const transFCS = require("./fc.json");
 
-// We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
+
 client.config = config;
 client.transFCS = transFCS;
 
